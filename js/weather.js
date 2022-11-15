08d111763ba489e199c33c267b8e901c2d58459f
chenyxy <2231497657@qq.com>
2022-11-15 11:02:16 +0800

    Site updated: 2022-11-15 11:02:16

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,25 @@
-WIDGET={CONFIG:{modules:"01234",background:"5",tmpColor:"4A86E8",tmpSize:"16",cityColor:"FF9900",citySize:"16",aqiColor:"4A86E8",aqiSize:"16",weatherIconSize:"24",alertIconSize:"18",padding:"0px 0px 0px 0px",shadow:"0",language:"auto",fixed:"true",vertical:"center",horizontal:"center",left:"125",top:"20",key:"2196ac4561e0402e80e40366c6326b2e"}};
+WIDGET = {
+
+"CONFIG": {
+
+"modules": "01234",
+"background": "5",
+"tmpColor": "4A86E8",
+"tmpSize": "16",
+"cityColor": "FF9900",
+"citySize": "16",
+"aqiColor": "4A86E8",
+"aqiSize": "16",
+"weatherIconSize": "24",
+"alertIconSize": "18",
+"padding": "0px 0px 0px 0px",
+"shadow": "0",
+"language": "auto",
+"fixed": "true",
+"vertical": "center",
+"horizontal": "center",
+"left": "125",
+"top": "20",
+"key": "2196ac4561e0402e80e40366c6326b2e"
+}
+}
